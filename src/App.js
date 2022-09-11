03bdcb5a36845ccfa0441993bde3e3fed1fbf7c8
Nathan Freeman <nathanlfreeman@gmail.com>
2022-09-11 00:26:17 -0400

    created hamburger menu and transition toggle effect, looks cool

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+// CSS
 import './App.css'
 
+//Component Imports
 import Header from './components/header/header'
 
 const App = () => {
   return (
     <>
-        <h1>React is working</h1>
         <Header />
     </>
   )
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,31 @@
+import { useState } from "react"
+import './header.css' 
+
 const Header = () => {
+    const [mobileMode, setMobileMode] = useState(false)
+    const [seeMenu, setSeeMenu] = useState(false)
+
+    const toggleMenu = () => {
+        const line1 = document.querySelector('.line-1')
+        const line2 = document.querySelector('.line-2')
+        const line3 = document.querySelector('.line-3')
+        line1.classList.toggle('line-1-active')
+        line2.classList.toggle('line-2-active')
+        line3.classList.toggle('line-3-active')
+        setSeeMenu(!seeMenu)
+    }
+
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        
+        <div className="mobile-header">
+            <h1><a href="#top">HKDL Caroler Info 2022</a></h1>
+            {/* Hamburger Menu icon */}
+            <div className="hamburger" onClick={toggleMenu}>
+                <div className="burger-line line-1"></div>
+                <div className="burger-line line-2"></div>
+                <div className="burger-line line-3"></div>
+            </div>
+        </div>
     )
 }
 
