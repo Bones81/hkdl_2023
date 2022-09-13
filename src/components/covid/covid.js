190959e216ae9ce626aec8084ec242230c7c3bb2
Nathan Freeman <nathanlfreeman@gmail.com>
2022-09-13 00:55:01 -0400

    update covid, flying, housing, and voting content; change text color for many elements

--- src/components/covid/covid.js
+++ src/components/covid/covid.js
@@ -4,10 +4,10 @@ const Covid = () => {
     return (
         <section id="covid-clearance">
             <h2 >COVID-related flying info and Cheat Sheet for COVID clearance</h2>
-            <h3 className='special-note'>This section is only partially updated for 2022. Double-check all official COVID-clearance information for the most accurate and up-to-date info.</h3>
+            <h3 className='special-note'>This section is only partially updated for 2022. For the time being, much of this section is copy/pasted from 2020. Double-check official COVID-clearance resources for the most accurate and up-to-date info.</h3>
                 <h4 class="special-note">We are responsible for finding a facility that will give us a nucleic acid COVID test within 48 hours of the departure time of our HK flight. That facility must therefore be able to get you the report and results you need before you fly out. Airlines receive stiff penalties--thousands of dollars--if they do not adhere to these requirements. So you <em>will not be allowed on the plane</em> if your COVID clearance docs are not in order! Yes, this part is stressful! One of our sopranos in 2020 was not allowed onto the plane at LAX because her lab report did not use the exact phrasing HK's COVID clearance calls for. (She ended up getting another test in LA and joining us in quarantine 2 days later.)</h4>
                 <h3>The original HK govt document about COVID clearance is <a target="_blank" rel='noreferrer' href="https://www.coronavirus.gov.hk/eng/high-risk-places.html">here</a>. My cheat sheet version of this info is what follows below.</h3>
-                <p>Short version - You need a qualifying test report, a qualifying lab certificate, and a 14-day hotel confirmation (which Disney will provide).</p>
+                <p>Short version - You need a qualifying test report, a qualifying lab certificate, and a quarantine hotel confirmation (which Disney will provide).</p>
                 <p>Longer version - In order to be allowed onto our flights to HK, we are required to provide to the airline (and later to HK Immigration) the following:</p>
                 <ol className='covid-ol'>
                     <li>A <strong>test report bearing your name exactly as it appears in your passport</strong> showing that:
@@ -22,7 +22,7 @@ const Covid = () => {
                     <li>If, for some reason, your test report above does not include all the requested info, then you must produce a <strong>written confirmation from the lab or doctor's office of all the relevant info above</strong> (i.e. tested within 48 hours of flight, showed it was a nucleic acid test, and you tested negative). That confirmation must <strong>include your name as it appears on your passport and clearly be from the lab or doctor's office</strong> (on their letterhead, ideally). Ideally it also includes a stamp or seal (or in Chinese terms, a "chop") from the lab or doctor's office. Again, this document is only required if doc #1 is incomplete.</li>
                     <li><strong>Document proving that the lab or doctor's office is ISO 15189 accredited or is recognized by the US or state health department.</strong> Every successful US-HK travel case I have researched indicates that lab certificates with a <strong><em>CLIA number</em></strong> satisfy this requirement. Here is an <a target="_blank" rel='noreferrer' href="https://ultimatedx.com/wp-content/uploads/2020/05/License-Certificate-2020.pdf">example of a certificate that includes a CLIA number</a>. Sometimes, if your facility cannot provide you a copy of their lab certification, you can find it online. However, if your facility is familiar with travel-related testing, they should understand why you need this document. 
                         <p class="special-note">Here is the <a target="_blank" rel='noreferrer' href="https://www.cms.gov/Regulations-and-Guidance/Legislation/CLIA/CLIA_Laboratory_Demographic_Information">US govt website that allows you to search for labs that have CLIA certification. </a> Take note that the name of the lab your testing facility uses to analyze your test may not be the same as the name of the testing facility itself.</p></li>
-                    <li><strong>Confirmation of a 14-day hotel reservation</strong> in HK for quarantining. <strong>Disney has said they will provide this info to us</strong> once our flights and hotels are booked.</li>
+                    <li><strong>Confirmation of a hotel reservation</strong> in HK for the quarantine period. <strong>Disney has said they will provide this info to us</strong> once our flights and hotels are booked.</li>
                 </ol>
                 <h4>Considering the above requirements, the key questions to ask when looking for a testing facility are:</h4>
                 <ul className='covid-ul'>
@@ -72,7 +72,7 @@ const Covid = () => {
                             </ul>
                         
                         <li>A <strong>lab certificate</strong> issued by your State Health Department that <em>includes a CLIA number</em>.</li>
-                        <li><strong>Proof of a 14-day hotel reservation</strong> in HK. Disney will provide this to us.</li>
+                        <li><strong>Proof of a quarantine hotel reservation</strong> in HK. Disney will provide this to us.</li>
                         
                     </ol>
                 </h4>
@@ -82,7 +82,7 @@ const Covid = () => {
                 </p>
                 <h4 class="special-note">It is possible you will have to spend one night in a hotel that is NOT your quarantine hotel. If you arrive to the airport so late that your COVID test cannot be analyzed before the end of the day, you will likely be taken to a separate hotel to spend the night and then will have to return to the waiting room the next day to await your confirmed test results. Only after your test results are confirmed will you be guided to the quarantine hotel provided by Disney. (You are still required to have a reservation at the Disney hotel for that first night, even if you end up staying in the airport-provided hotel due to a late arrival. Yes, that's an unfair rule, but it doesn't really affect us, since Disney is paying for the hotel.)</h4>
                 <p>Morgan's report via our friend in HK and former face character, Rachel Capp: "We will be at the airport for like 3-4 hours once we land in their facility hall to get tested, get our wellness app together and basically a house arrest bracelet for quarantine so just mentally prepare now it might be a long day. So I’d pack snacks and like things to make the waiting more comfortable."</p>
-                <p>I've also seen many people report that the waiting hall is heavily air conditioned, so be sure to have a sweater or light jacket in your carry-on luggage. Some have suggested bringing a yoga mat so that you can lie down, rather than sit in the same plastic chair for 3-4 hours.</p>
+                <p>The waiting hall is well air conditioned, so be sure to have a sweater or light jacket in your carry-on luggage. Some have suggested bringing a yoga mat so that you can lie down, rather than sit in the same plastic chair for 3-4 hours.</p>
                 <p>Other comments/recommendations for the waiting hall in HK: bring 1-2 fully charged power banks for your phone, as the availability of electrical outlets is limited. WiFi is "okay." They had a small selection of snack items (Oreos, peanut butter crackers, etc.), but other than that, you'll need your own food.</p>
         </section>
     )
