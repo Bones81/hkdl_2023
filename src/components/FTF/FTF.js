8968ade1578b3bdee690cc8a77edf3540cf0e0c3
Nathan Freeman <nathanlfreeman@gmail.com>
2022-09-12 22:43:44 -0400

    add 2022 disclaimer to FTF component; minor covid text update

--- src/components/FTF/FTF.js
+++ src/components/FTF/FTF.js
@@ -6,6 +6,7 @@ const FTF = () => {
                 <p>Welcome, carolers! I thought a reference website could be helpful as newbies and vets alike try to learn/remember what this contract requires of us.</p>
                 <p>First off, congratulations on earning this contract. Almost everyone who has done it considers it a highlight of their career and life. Hong Kong is an amazing city, and the people are equally amazing. There are too many good things about this contract to cover in one place!</p>
                 <p>You'll have <b>A HUGE AMOUNT</b> of information thrown at you between now and the end of the contract, and the hope is that this site collects and organizes as much of that info as possible.</p>
+                <h3 className="special-note">I've done my best to update everything on this site for 2022. That said, you may well encounter information that is leftover from 2020. If anything seems out-of-date, it may well be. Let me know if anything is clearly inaccurate.</h3>
                 <h4 className="special-note">To be clear, this site is in no way an official Disney or Goode Time resource or stamped with anyone's seal of approval. It is merely an attempt by previous HKDL carolers to consolidate and digest the massive amount of information and advice we receive related to this contract. If this site's info conflicts with any official Disney or Goode Time info, please prioritize what Disney or Goode Time says. Everything is subject to change!</h4>
         </section>
     )
