23406f379e511823a72d02b0d1bec4d8c0906d64
Nathan Freeman <nathanlfreeman@gmail.com>
2022-10-01 12:12:13 -0400

    update housing

--- src/components/housing/housing.js
+++ src/components/housing/housing.js
@@ -4,24 +4,12 @@ const Housing = () => {
     return (
         <section id='housing'>
             <h2>Housing</h2>
-            <h3>Potential one-night stay in airport-provided hotel while you wait for your COVID test results.</h3>
-                <p>If you arrive too late in the day for your COVID test results to be available before the end of the day, you will be assigned to a hotel for one night and have to wait until your test results are confirmed. Note that this will not be your quarantine hotel and that you must still have a reservation at your quarantine hotel for the night you arrive, even if you end up staying in the airport-provided hotel instead.</p>
-            <h3>Unknown hotel, our quarantine home</h3>
-                <p>Won't be able to leave the room for any reason for 3 days, with the possibility that quarantine could be lifted early. After those 3 days, we will be expected to self-isolate for 4 days.</p>
-                <p>A key moment will be when we first arrive into this "quarantine home." Almost immediately upon arrival, we will have to activate our StayHomeSafe app, which will give us one minute--or possibly even less--to walk the perimeter of our hotel room so that the GPS knows where we will be for the next 3 days. Yikes! Make sure you cover the entire perimeter of your room quickly without running (Apparently the GPS doesn't work right if you run because it measures data every few steps.), or else you'll have to contact someone to help you reset your perimeter.</p>
-                <p>When we quarantined in 2020, 3 meals were delivered to our room each day, but we had the option to order delivery on our own dime from a variety of restaurants via FoodPanda (which is like GrubHub).</p>
-                <p>Additionally, in 2020 Disney provided us with a care package in our room that had a ton of snacks (including copious amounts of whole fruit) and some basic cleaning supplies. Since our quarantine is much shorter this year, we may not get such a substantial care package, if any at all.</p>
-                <p>We will likely have to take and record our temperature daily. My understanding is that we will be issued a thermometer during our airport HK COVID test. However, if the thermometer stops working, we are still expected to record our temperature, so it is not a bad idea to have your own thermometer handy so that you don't have to explain why you didn't record your temperature.</p>
-                <p>My understanding is also that we will have to take another COVID test at some point.</p>
-                <p>If you need to do laundry during quarantine, you will probably have to do it by hand in your bathtub, or else it's not happening. Consider bringing some soap or laundry detergent pods.</p>
-                <p>I've read differing reports on wifi quality in quarantine hotels. In 2020, our wifi was pretty good. Hopefully this year our wifi is also good enough that we can Zoom and Facetime without much worry.</p>
-                <p><a href="https://cassidystheprincessdiaries.blogspot.com/2020/05/you-are-dancing-queen-young-and-sweet.html" target="_blank" rel='noreferrer'>Here is a link</a> to a blog by one of the princesses at HKDL, detailing her experience with the 14-day quarantine period in 2020. She discusses temperature checks, meals, and her accommodations. <a href="https://cassidystheprincessdiaries.blogspot.com/2020/05/i-am-kind-of-starting-to-understand-why.html" target="_blank" rel='noreferrer'>Here is another.</a> Ours may be the same hotel, or it may not.</p>
             <h3 id="HPRC"><a href="https://www.harbour-plaza.com/resortcity/Index-en.htm">Harbour Plaza Resort City, or HPRC</a></h3>
                 <img id="HPRC-lobby" src="./assets/HPRC lobby.jpg" alt="The lobby of HPRC" />
                 <p className="img-caption">The lobby of our HK home, HPRC.</p>
                 <p>Located in the Tin Shui Wai neighborhood, very close to the border with mainlaind China and the 12.5 million-person city Shenzhen, <a target="_blank"  rel='noreferrer'href="https://www.harbour-plaza.com/resortcity/Index-en.htm">HPRC</a> is our home in HK. All of HKDL's expats are housed there.</p>
                 <img id="viewfromHPRC" src="./assets/viewfromHPRC.jpg" alt="The view from a room at Harbour Plaza Resort City" />
-                <p className="img-caption">The view from a room at Harbour Plaza Resort City, where we live for the majority of the contract.</p>
+                <p className="img-caption">The view from a room at Harbour Plaza Resort City, where we live during the contract.</p>
                 <h4>Accommodations</h4>
                     <video id="HPRC-room-tour" width= "400" height="500" controls>
                         <source src="./assets/HPRC room first look.m4v" type="video/mp4"/>
