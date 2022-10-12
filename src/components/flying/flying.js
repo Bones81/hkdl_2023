2f1abb1d0f3735c3d2defef16d89f0cac9575afc
Nathan Freeman <nathanlfreeman@gmail.com>
2022-10-12 00:48:39 -0400

    update flight section post-flight info receipt

--- src/components/flying/flying.js
+++ src/components/flying/flying.js
@@ -1,24 +1,29 @@
 const Flying = () => {
     return (
         <section id='flights'>
-            <h2>Flights</h2>
-            <h3>When will we get the info?</h3>
-                <p>Traditionally, we receive our flight information within 72 hours of our flight date. Our flight date this year should be Saturday, Oct. 29th, the same as the first day of our contract. If there is a delay in processing our visas, we may be forced to depart at a later date (This scenario happened in 2020).</p>
-                {/* <p>We will likely not receive our flight info before Sept. 13th-15th or so. And according to Disney, we will not receive any flight info until they have confirmed that we have received our travel visas.</p> */}
-                <p>Check with your fellow carolers. If they have all received their flight info and you have not, it is worth following up with HK Disney HR to ensure your flight info is on its way.</p>
-                <h4 class="special-note">IMPORTANT NOTE: If you end up having a connecting flight to HK, your pre-flight RAT COVID test (see above section on <a href="#covid-clearance">COVID clearance</a>) must occur within 24 hours of the departure time of your <em>connecting</em> flight, not your first flight. As a result, you may have to reschedule your COVID test if it does not fall within 24 hours of the connecting flight's departure time. Take note as well that you would have to take time zone differences into account if your connecting flight does not leave from the same time zone as the one in which you are getting tested.</h4>
+            <h2>Flights -- <em>UPDATED 10/11/2022</em></h2>
+            <h3 className="special-note"><em>UPDATE: Oct 11 2022</em> Flight info has been issued!</h3>
+                <p>Looks like we're all on <a href="https://www.koreanair.com/us/en" target="_blank" rel="noreferrer">Korean Air</a>! Our respective flights all depart on Sunday, Oct 30th. Then we will connect to the same flight from Incheon airport in Seoul to Hong Kong. </p>
+                <h4 className="special-note">Important Note: Our RAT COVID test (see above section on <a href="#covid-clearance">COVID clearance</a>) must be within 24 hours of the flight from Seoul to Hong Kong. That flight leaves at 20:00 (i.e. 8pm) local time on Oct 31. <br/><br/>
+                Therefore, you must do your pre-flight rapid test and fill out the online HK health declaration form AFTER...</h4>
+                <ul>
+                    <li>4:00AM, Sunday Oct 30, Los Angeles time</li>
+                    <li>3:00AM, Sunday Oct 30, Anchorage time</li>
+                    <li>7:00AM, Sunday Oct 30, New York time</li>
+                </ul>
             <h3>Change your seat!</h3>
-                <p>Once Disney sends you your flight info, you may be able to change your seat to one you prefer. May as well be as comfortable as possible for the long flight(s)!</p>    
+                <p>You may be able to change your seat to one you prefer. May as well be as comfortable as possible for the long flight(s)!</p>    
             <h3>Baggage</h3>
-                <p>Your baggage allowance will be determined by the airline of your flight. Since Disney controls the booking process, you don't really have a say in which airline is chosen.</p>
-                <p>Historically, I have traveled with 2 checked rolling suitcases/bags, a medium-sized duffle bag, and a medium-sized backpack. It's basically as much as I can handle by myself. But remember, you can buy just about ANYTHING in HK, so if you don't need it, consider leaving it at home. Keep your luggage manageable. Number one rule of traveling: don't overpack.</p>
+                <p>Looks like we get two complimentary checked bags up to 50lbs, a 1 bag + 1 personal item to carry on (total carry on weight is 22lbs). The fees are hefty for exceeding the weight limits.</p>
+                <p>Historically, I have traveled with 2 checked rolling suitcases/bags, a medium-sized duffle bag, and a medium-sized backpack. It's basically as much as I can handle by myself. But remember, you can buy just about ANYTHING in HK, so if you don't need it, leave it at home. Number one rule of traveling: don't overpack.</p>
                 <p>It is always possible that a checked bag could get lost, so it is extremely useful to have a full complement of essential clothes, food, chargers/power, and entertainment in your carry-on luggage.</p>
-            <h3>Luggage fees</h3>
-                <p>Any luggage fees for your flight will depend on the airline you are flying.</p>
+            <h3>In-flight meals</h3>
+                <p>Check your respective tickets for what meal service you'll be getting. For the final leg, we will get dinner. Here is <a href="https://www.koreanair.com/us/en/in-flight/onboard-service/dining/economy" target="_blank" rel="noreferrer">Korean Air's info on in-flight meals</a>, including options for <a href="https://www.koreanair.com/us/en/in-flight/onboard-service/dining/special-meals" target="_blank" rel="noreferrer">special meal requests</a>.</p>
             <h3>In-flight entertainment</h3>
-                <p>Once you know your airline, you might want to check their website to determine whether they require you to download an app to access in-flight movies/entertainment. I believe such an app is necessary on United flights. Cathay Pacific, on the other hand, does not require you to download an app.</p>
+                <p>Korean Air's <a href="https://www.koreanair.com/in-flight/entertainment" target="_blank" rel="noreferrer" >in-flight entertainment options are here</a>.</p>
             <h3>Frequent Flier program</h3>
                 <p>A flight to HK is a huge number of miles! Take advantage of your airline's frequent flier program and ensure that your flight(s) counts toward your miles!</p>
+                <p>Here is a link to the <a href="https://www.koreanair.com/us/en/skypass/status-benefits/status-benefits/grade/skypass-club" target="_blank" rel="noreferrer">Korean Air frequent flier program</a>. Looks like there might be a way to connect to Delta or Alaska Airlines miles??  </p>
             <h3>Mask recommendation</h3>
                 <p><a target="_blank" rel="noreferrer" href="https://humidiflyer.com/">Humidiflyer</a>. Thanks for this rec, Joshua!</p>
         </section>
