2ac4baa801c984cb9e9ca44cf16f3e370a3ae3da
Nathan Freeman <nathanlfreeman@gmail.com>
2023-07-31 14:55:28 -0400

    update Flying section

--- src/components/flying/flying.js
+++ src/components/flying/flying.js
@@ -4,6 +4,14 @@ const Flying = () => {
         <h2 className="section-title">
           Flights
         </h2>
+        <h3>Flight info</h3>
+        <p>HKDL has historically waited until the last week -- and sometimes even the last 72 hours -- to give us our flight information. It may feel a bit nerve-wracking to be inching closer to the flight date without any itinerary, but just know the flight info will show up!</p>
+        <p>Because HK Disney has indicated the two groups will have different contract start dates, and because our flights are usually 1-2 days before the contract start date, the following scenario is likely:</p>
+        <ul>
+            <li>Group 1 (Katie, Emilia, Nathan, Sean): Flight date Oct 28 or 29, contract start Oct 30.</li>
+            <li>Group 2 (Sarah, Chelsea, Kyle, Johnny): Flight date Nov 4 or 5, contract start Nov 6.</li>
+        </ul>
+        <p>During the pandemic contracts, they were careful to connect all of us to the same Hong Kong flight, even though we come from all over the US. However, now that pandemic precautions are ended, I suspect they'll go back to the old system, where they would just have the group arrive to Hong Kong within a couple hours of each other and have Irene or someone from HR greet us in the airport arrivals hall, give us a welcome packet, and then set us up with taxis to HPRC. They would often time our arrival with additional expat face characters also starting their contracts. They'll let us know the plan, but it may not be until the week we are scheduled to depart.</p>
         <h3>Change your seat!</h3>
         <p>
           You may be able to change your seat to one you prefer. May as well be as comfortable as possible for the
@@ -14,11 +22,11 @@ const Flying = () => {
         <h3>Baggage</h3>
         <p>
           Depending on what airline you are on, you usually get two complimentary checked bags up to 50lbs, as well as 1 bag
-          + 1 personal item to carry on (total carry on weight 22lbs). The
-          fees are hefty for exceeding the weight limits.
+          + 1 personal item to carry on (total carry-on weight 22lbs). The
+          fees are often hefty for exceeding the weight limits.
         </p>
         <p>
-          Historically, I have traveled with 2 checked rolling suitcases/bags, a
+          I have usually traveled with 2 checked rolling suitcases/bags, a
           medium-sized duffle bag, and a medium-sized backpack. It's basically
           as much as I can handle by myself. But remember, you can buy just
           about ANYTHING in HK, so if you don't need it, leave it at home.
@@ -29,6 +37,8 @@ const Flying = () => {
           extremely useful to have a full complement of essential clothes, food,
           chargers/power, and entertainment in your carry-on luggage.
         </p>
+        <h3>In-Flight Meals</h3>
+        <p>Be sure to check with your airline to arrange any special meals you may need/want.</p>
         <h3>Frequent Flier program</h3>
         <p>
           A flight to HK is a huge number of miles! Take advantage of your
